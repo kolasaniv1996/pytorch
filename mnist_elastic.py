7b4f60927504959d45f273c71d7f4a979d82187b
kolasaniv1996 <92003218+kolasaniv1996@users.noreply.github.com>
2024-07-15 11:33:01 -0400

    Update mnist_elastic.py

--- mnist_elastic.py
+++ mnist_elastic.py
@@ -1,11 +1,10 @@
 import os
 import torch
 import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, DistributedSampler
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 import argparse
 
 class Net(nn.Module):
@@ -24,27 +23,27 @@ class Net(nn.Module):
         x = self.fc2(x)
         return torch.log_softmax(x, dim=1)
 
-def train(rank, args):
-    dist.init_process_group(backend='nccl', init_method='env://')
-    torch.cuda.set_device(rank)
-    model = Net().to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
-    
+def train(args, rank, local_rank):
+    dist.init_process_group(backend='nccl')
+    torch.cuda.set_device(local_rank)
+    model = Net().to(local_rank)
+    ddp_model = nn.parallel.DistributedDataParallel(model, device_ids=[local_rank])
+
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
     dataset = datasets.MNIST('../data', train=True, download=True, transform=transform)
-    sampler = DistributedSampler(dataset)
-    train_loader = DataLoader(dataset, batch_size=args.batch_size, sampler=sampler)
-    
+    train_sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=args.world_size, rank=rank)
+    train_loader = DataLoader(dataset, batch_size=args.batch_size, sampler=train_sampler)
+
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.01, momentum=0.9)
-    
+
     for epoch in range(1, args.epochs + 1):
         ddp_model.train()
-        sampler.set_epoch(epoch)
+        train_sampler.set_epoch(epoch)
         for batch_idx, (data, target) in enumerate(train_loader):
-            data, target = data.cuda(rank), target.cuda(rank)
+            data, target = data.cuda(local_rank), target.cuda(local_rank)
             optimizer.zero_grad()
             output = ddp_model(data)
             loss = nn.functional.nll_loss(output, target)
@@ -57,10 +56,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch-size', type=int, default=64, help='input batch size for training')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train')
+    parser.add_argument('--world-size', type=int, default=int(os.environ.get('WORLD_SIZE', 1)), help='number of distributed processes')
+    parser.add_argument('--rank', type=int, default=int(os.environ.get('RANK', 0)), help='rank of this process')
+    parser.add_argument('--local-rank', type=int, default=int(os.environ.get('LOCAL_RANK', 0)), help='local rank of this process')
     args = parser.parse_args()
 
-    world_size = int(os.environ['WORLD_SIZE'])
-    rank = int(os.environ['RANK'])
-    local_rank = int(os.environ['LOCAL_RANK'])
-
-    torch.multiprocessing.spawn(train, nprocs=world_size, args=(args,))
+    train(args, args.rank, args.local_rank)
